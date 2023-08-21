7146e741d2376b5df0a3c5b01d9c69c427035a6d
tfkhdyt <tfkhdyt@proton.me>
2023-08-21 20:06:43 +0700

    feat(user): add repository interface

---
+++ backend/internal/domain/repository/user_repo.go
@@ -0,0 +1,17 @@
+package repository
+
+import "github.com/tfkhdyt/SpaceNotes/backend/internal/domain/entity"
+
+type UserRepo interface {
+	CreateUser(user *entity.NewUser) (*entity.CreatedUser, error)
+
+	FindUserByID(id int) (*entity.User, error)
+	FindUserByUsername(username string) (*entity.User, error)
+	FindUserByEmail(email string) (*entity.User, error)
+
+	UpdateUser(id int, data *entity.UpdateUser) (*entity.UpdatedUser, error)
+	UpdateEmail(id int, email string) (*entity.UpdatedUser, error)
+	UpdatePassword(id int, password string) error
+
+	DeleteUser(id int) error
+}
