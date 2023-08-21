f967d404b80456a36d39d82d26a1a5522671a46f
tfkhdyt <tfkhdyt@proton.me>
2023-08-21 20:07:27 +0700

    feat(user): add dto

---
+++ backend/internal/application/dto/user_dto.go
@@ -0,0 +1,40 @@
+package dto
+
+import "github.com/tfkhdyt/SpaceNotes/backend/internal/domain/entity"
+
+type (
+	RegisterRequest      = entity.NewUser
+	RegisterResponseData = entity.CreatedUser
+	RegisterResponse     struct {
+		Message string               `json:"message"`
+		Data    RegisterResponseData `json:"data"`
+	}
+
+	FindUserByIDResponseData = entity.User
+	FindUserByIDResponse     struct {
+		Data FindUserByIDResponseData `json:"data"`
+	}
+
+	UpdateUserRequest      = entity.UpdateUser
+	UpdateUserResponseData = entity.UpdatedUser
+	UpdateUserResponse     struct {
+		Message string                 `json:"message"`
+		Data    UpdateUserResponseData `json:"data"`
+	}
+
+	UpdateEmailRequest struct {
+		NewEmail string `json:"new_email"`
+		Password string `json:"password"`
+	}
+
+	UpdatePasswordRequest struct {
+		OldPassword     string `json:"old_password"`
+		NewPassword     string `json:"new_password"`
+		ConfirmPassword string `json:"confirm_password"`
+	}
+	UpdatePasswordResponse struct {
+		Message string `json:"message"`
+	}
+
+	DeleteUserResponse = UpdatePasswordResponse
+)
