237c8c8c57dae6b8d636088892ef6908838c75d7
tfkhdyt <tfkhdyt@proton.me>
2023-08-23 07:21:11 +0700

    feat(controller/user): add update and delete controller

--- server/internal/interface/api/controller/user_controller.go
+++ server/internal/interface/api/controller/user_controller.go
@@ -3,20 +3,18 @@ package controller
 import (
 	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
-	"github.com/tfkhdyt/SpaceNotes/backend/internal/application/dto"
-	"github.com/tfkhdyt/SpaceNotes/backend/internal/application/usecase"
-	"github.com/tfkhdyt/SpaceNotes/backend/pkg/exception"
+	"github.com/tfkhdyt/SpaceNotes/server/internal/application/dto"
+	"github.com/tfkhdyt/SpaceNotes/server/internal/application/usecase"
+	"github.com/tfkhdyt/SpaceNotes/server/pkg/auth"
+	"github.com/tfkhdyt/SpaceNotes/server/pkg/exception"
 )
 
 type UserController struct {
 	userUsecase *usecase.UserUsecase `di.inject:"userUsecase"`
 }
 
-func (u *UserController) FindUserByID(c *fiber.Ctx) error {
-	userID, err := c.ParamsInt("userID")
-	if err != nil {
-		return exception.NewHTTPError(400, "Invalid user id")
-	}
+func (u *UserController) FindMyAccount(c *fiber.Ctx) error {
+	userID := auth.GetUserIDFromClaims(c)
 
 	result, errFind := u.userUsecase.FindUserByID(userID)
 	if errFind != nil {
@@ -26,11 +24,8 @@ func (u *UserController) FindUserByID(c *fiber.Ctx) error {
 	return c.JSON(result)
 }
 
-func (u *UserController) UpdateUser(c *fiber.Ctx) error {
-	userID, err := c.ParamsInt("userID")
-	if err != nil {
-		return exception.NewHTTPError(400, "Invalid user id")
-	}
+func (u *UserController) UpdateMyAccount(c *fiber.Ctx) error {
+	userID := auth.GetUserIDFromClaims(c)
 
 	payload := new(dto.UpdateUserRequest)
 	if err := c.BodyParser(payload); err != nil {
@@ -44,6 +39,57 @@ func (u *UserController) UpdateUser(c *fiber.Ctx) error {
 	result, errUpdate := u.userUsecase.UpdateUser(userID, payload)
 	if errUpdate != nil {
 		return errUpdate
+	}
+
+	return c.JSON(result)
+}
+
+func (u *UserController) UpdateMyEmail(c *fiber.Ctx) error {
+	userID := auth.GetUserIDFromClaims(c)
+
+	payload := new(dto.UpdateEmailRequest)
+	if err := c.BodyParser(payload); err != nil {
+		return exception.NewHTTPError(422, "Failed to parse body")
+	}
+
+	if _, err := govalidator.ValidateStruct(payload); err != nil {
+		return exception.NewValidationError(err)
+	}
+
+	result, err := u.userUsecase.UpdateEmail(userID, payload)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(result)
+}
+
+func (u *UserController) UpdateMyPassword(c *fiber.Ctx) error {
+	userID := auth.GetUserIDFromClaims(c)
+
+	payload := new(dto.UpdatePasswordRequest)
+	if err := c.BodyParser(payload); err != nil {
+		return exception.NewHTTPError(422, "Failed to parse body")
+	}
+
+	if _, err := govalidator.ValidateStruct(payload); err != nil {
+		return exception.NewValidationError(err)
+	}
+
+	result, err := u.userUsecase.UpdatePassword(userID, payload)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(result)
+}
+
+func (u *UserController) DeleteMyAccount(c *fiber.Ctx) error {
+	userID := auth.GetUserIDFromClaims(c)
+
+	result, err := u.userUsecase.DeleteUser(userID)
+	if err != nil {
+		return err
 	}
 
 	return c.JSON(result)
