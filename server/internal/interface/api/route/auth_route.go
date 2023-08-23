45514ab42c334c0582df48b912669d4a87bd2279
tfkhdyt <tfkhdyt@proton.me>
2023-08-23 07:21:47 +0700

    feat(route): register auth routes

---
+++ server/internal/interface/api/route/auth_route.go
@@ -0,0 +1,14 @@
+package route
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/tfkhdyt/SpaceNotes/server/internal/interface/api/controller"
+)
+
+type AuthRoute struct {
+	authController *controller.AuthController `di.inject:"authController"`
+}
+
+func (a *AuthRoute) RegisterRoute(r fiber.Router) {
+	r.Post("/register", a.authController.Register)
+}
