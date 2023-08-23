c0f6689beeafa9e1005fe4746a838f3a6dc865c7
tfkhdyt <tfkhdyt@proton.me>
2023-08-23 09:18:25 +0700

    refactor(middleware/auth): use jwt access token key from config module

--- server/internal/interface/api/middleware/auth_middleware.go
+++ server/internal/interface/api/middleware/auth_middleware.go
@@ -1,17 +1,14 @@
 package middleware
 
 import (
-	"os"
-
 	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/tfkhdyt/SpaceNotes/server/config"
 	"github.com/tfkhdyt/SpaceNotes/server/pkg/exception"
 )
 
-var jwtAccessTokenKey = os.Getenv("JWT_ACCESS_TOKEN_KEY")
-
 var JwtMiddleware = jwtware.New(jwtware.Config{
-	SigningKey:  jwtware.SigningKey{Key: []byte(jwtAccessTokenKey)},
+	SigningKey:  jwtware.SigningKey{Key: []byte(config.JwtAccessTokenKey)},
 	TokenLookup: "header:Authorization,cookie:accessToken",
 	AuthScheme:  "Bearer",
 	ErrorHandler: func(c *fiber.Ctx, err error) error {
