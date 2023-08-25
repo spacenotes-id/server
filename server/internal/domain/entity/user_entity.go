df81ee021ec5f8df340d39b70516e5d760d951d9
tfkhdyt <tfkhdyt@proton.me>
2023-08-25 10:16:20 +0700

    feat(entity/user): add json tags, and change FullName type to string or sql.NullString

--- server/internal/domain/entity/user_entity.go
+++ server/internal/domain/entity/user_entity.go
@@ -1,42 +1,45 @@
 package entity
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type User struct {
-	ID        int
-	FullName  *string
-	Username  string
-	Email     string
-	Password  string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        int            `json:"id"`
+	FullName  sql.NullString `json:"full_name"`
+	Username  string         `json:"username"`
+	Email     string         `json:"email"`
+	Password  string         `json:"password"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
 }
 
 type NewUser struct {
-	FullName *string `json:"full_name" valid:"stringlength(2|70)~Full name length should be at least 2 - 70 characters"`
-	Username string  `json:"username" valid:"required~Username is required,stringlength(3|16)~Username length should be at least 3 - 16 characters"`
-	Email    string  `json:"email" valid:"required~Email is required,email~Invalid email"`
-	Password string  `json:"password" valid:"required~Password is required"`
+	FullName string `json:"full_name" valid:"stringlength(2|70)~Full name length should be at least 2 - 70 characters"`
+	Username string `json:"username" valid:"required~Username is required,stringlength(3|16)~Username length should be at least 3 - 16 characters"`
+	Email    string `json:"email" valid:"required~Email is required,email~Invalid email"`
+	Password string `json:"password" valid:"required~Password is required"`
 }
 
 type CreatedUser struct {
-	ID        int
-	FullName  *string
-	Username  string
-	Email     string
-	CreatedAt time.Time
+	ID        int       `json:"id"`
+	FullName  string    `json:"full_name"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type UpdateUser struct {
-	FullName *string
-	Username *string
+	FullName string `json:"full_name" valid:"stringlength(2|70)~Full name length should be at least 2 - 70 characters"`
+	Username string `json:"username" valid:"stringlength(3|16)~Username length should be at least 3 - 16 characters"`
 }
 
 type UpdatedUser struct {
-	ID        int
-	FullName  *string
-	Username  string
-	Email     string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        int       `json:"id"`
+	FullName  string    `json:"full_name"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
