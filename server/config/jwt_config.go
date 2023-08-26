9d429049be03c1b30e5bcb7174c7ba7f67fd80cd
tfkhdyt <me@tfkhdyt.my.id>
2023-08-26 07:23:38 +0700

    feat(config/jwt): add refresh token key

--- server/config/jwt_config.go
+++ server/config/jwt_config.go
@@ -2,4 +2,7 @@ package config
 
 import "os"
 
-var JwtAccessTokenKey = os.Getenv("JWT_ACCESS_TOKEN_KEY")
+var (
+	JwtAccessTokenKey  = os.Getenv("JWT_ACCESS_TOKEN_KEY")
+	JwtRefreshTokenKey = os.Getenv("JWT_REFRESH_TOKEN_KEY")
+)
