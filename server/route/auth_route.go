6c6b65bdef539c107a663bdfa37620e3ec79dbba
tfkhdyt <me@tfkhdyt.my.id>
2023-08-27 17:08:57 +0700

    feat(route/auth): add logout route

--- server/route/auth_route.go
+++ server/route/auth_route.go
@@ -3,6 +3,7 @@ package route
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/tfkhdyt/SpaceNotes/server/controller"
+	"github.com/tfkhdyt/SpaceNotes/server/middleware"
 )
 
 type AuthRoute struct {
@@ -12,4 +13,5 @@ type AuthRoute struct {
 func (a *AuthRoute) RegisterRoute(r fiber.Router) {
 	r.Post("/register", a.authController.Register)
 	r.Post("/login", a.authController.Login)
+	r.Delete("/logout", middleware.JwtMiddleware, a.authController.Logout)
 }
