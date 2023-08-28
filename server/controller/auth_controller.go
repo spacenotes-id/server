a7f4414d90ccc4db0f90305a39aee3b9a91b6ff4
tfkhdyt <me@tfkhdyt.my.id>
2023-08-28 19:34:38 +0700

    feat(controller/auth): add Refresh handler

--- server/controller/auth_controller.go
+++ server/controller/auth_controller.go
@@ -68,24 +68,33 @@ func (a *AuthController) Login(c *fiber.Ctx) error {
 	return c.Status(201).JSON(result)
 }
 
-func (a *AuthController) Logout(c *fiber.Ctx) error {
+func (a *AuthController) getRefreshToken(c *fiber.Ctx) (string, error) {
 	payload := new(dto.LogoutRequest)
 	if err := c.BodyParser(payload); err != nil {
 		rfrsh := c.Cookies("refreshToken")
 		if rfrsh == "" {
-			return fiber.NewError(fiber.StatusUnauthorized, "Invalid refresh token")
+			return "", fiber.NewError(fiber.StatusUnauthorized, "Invalid refresh token")
 		}
 
 		payload.RefreshToken = rfrsh
 	}
 
 	if _, err := govalidator.ValidateStruct(payload); err != nil {
-		return exception.NewValidationError(err)
+		return "", exception.NewValidationError(err)
 	}
 
-	response, err := a.authUsecase.Logout(payload.RefreshToken)
+	return payload.RefreshToken, nil
+}
+
+func (a *AuthController) Logout(c *fiber.Ctx) error {
+	refreshToken, err := a.getRefreshToken(c)
 	if err != nil {
 		return err
+	}
+
+	response, errLogout := a.authUsecase.Logout(refreshToken)
+	if errLogout != nil {
+		return errLogout
 	}
 
 	c.Cookie(&fiber.Cookie{
@@ -101,6 +110,28 @@ func (a *AuthController) Logout(c *fiber.Ctx) error {
 		Value:    "deleted",
 		Path:     "/",
 		Expires:  time.Date(2002, time.April, 1, 23, 0, 0, 0, time.UTC),
+		HTTPOnly: true,
+	})
+
+	return c.JSON(response)
+}
+
+func (a *AuthController) Refresh(c *fiber.Ctx) error {
+	refreshToken, err := a.getRefreshToken(c)
+	if err != nil {
+		return err
+	}
+
+	response, errRefresh := a.authUsecase.Refresh(refreshToken)
+	if errRefresh != nil {
+		return errRefresh
+	}
+
+	c.Cookie(&fiber.Cookie{
+		Name:     "accessToken",
+		Value:    response.Data.AccessToken,
+		Path:     "/",
+		Expires:  time.Now().Add(15 * time.Minute),
 		HTTPOnly: true,
 	})
 
