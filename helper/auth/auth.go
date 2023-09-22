4e2c107464a8a5b4f044ac9bb86fc5600ac576a0
tfkhdyt <me@tfkhdyt.my.id>
2023-09-22 11:50:04 +0700

    fix(helper/auth): return error if failed to get user id

--- helper/auth/auth.go
+++ helper/auth/auth.go
@@ -5,9 +5,15 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func GetUserIDFromClaims(c *fiber.Ctx) int {
-	user := c.Locals("user").(*jwt.Token)
-	claims := user.Claims.(jwt.MapClaims)
+func GetUserIDFromClaims(c *fiber.Ctx) (int, error) {
+	user, ok := c.Locals("user").(*jwt.Token)
+	if !ok {
+		return 0, fiber.NewError(fiber.StatusBadRequest, "Failed to get user id from claims")
+	}
+	claims, ok2 := user.Claims.(jwt.MapClaims)
+	if !ok2 {
+		return 0, fiber.NewError(fiber.StatusBadRequest, "Failed to get user id from claims")
+	}
 
-	return int(claims["user_id"].(float64))
+	return int(claims["user_id"].(float64)), nil
 }
