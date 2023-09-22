21f0d15e55882dc2ab4bd43de8a7d0905c616a86
tfkhdyt <me@tfkhdyt.my.id>
2023-09-22 11:52:43 +0700

    fix(middleware/note): add error handling to GetUserID helper

--- middleware/note_middleware.go
+++ middleware/note_middleware.go
@@ -3,12 +3,17 @@ package middleware
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/goioc/di"
+
 	"github.com/spacenotes-id/server/helper/auth"
 	"github.com/spacenotes-id/server/usecase"
 )
 
 func NoteOwnership(c *fiber.Ctx) error {
-	userID := auth.GetUserIDFromClaims(c)
+	userID, err := auth.GetUserIDFromClaims(c)
+	if err != nil {
+		return err
+	}
+
 	noteID, err := c.ParamsInt("note_id")
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, "Failed to get note id")
--- middleware/space_middleware.go
+++ middleware/space_middleware.go
@@ -3,12 +3,17 @@ package middleware
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/goioc/di"
+
 	"github.com/spacenotes-id/server/helper/auth"
 	"github.com/spacenotes-id/server/usecase"
 )
 
 func SpaceOwnership(c *fiber.Ctx) error {
-	userID := auth.GetUserIDFromClaims(c)
+	userID, err := auth.GetUserIDFromClaims(c)
+	if err != nil {
+		return err
+	}
+
 	spaceID, err := c.ParamsInt("space_id")
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, "Failed to get space id")
